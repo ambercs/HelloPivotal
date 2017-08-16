1310ebba08c4c7035388137f2ce7c87406b23aa3
Isuru Amarathunga <isuama.amarathunga@gmail.com>
2017-08-16 17:31:50 +0530

    Initial Checking

---
+++ WebPivotal/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace WebPivotal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            string domain = "http://localhost:50324/";
+            string url = "api/values";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                ViewData["Message"] = response.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                ViewData["Message"] = "Failed";
+            }
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
